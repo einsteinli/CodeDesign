590c329928c7b1e40f0e50d786e243e2059485c8
einsteinli <42836337+einsteinli@users.noreply.github.com>
2018-09-01 14:11:00 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<header>
+  <h1>Hello World</h1>
+</header>
+<body>
+  <p>ich bin ein Text <br> und stehe im body</p>
+  <?php
+  $variable = "php ist doll";
+  echo $variable
+  ?>
+</body>
+</html>
